d91d9830536b7d3b2467e310dccd0a758cfcf8a3
DrDerete <122503880+DrDerete@users.noreply.github.com>
2024-09-25 15:25:25 +0300

    1

---
+++ StochasticWay/Cell.java
@@ -0,0 +1,18 @@
+package StochasticWay;
+
+public class Cell {
+
+    private String name;
+
+    public Cell(String n) {
+        this.name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ StochasticWay/Main.java
@@ -0,0 +1,7 @@
+package StochasticWay;
+
+public class Main {
+    public static void main(String[] args) {
+         Environment env = new Environment(3, 4);
+    }
+}
