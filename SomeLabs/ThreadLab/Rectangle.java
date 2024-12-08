9bc6aa0f3b3e6f1a711efa0daf7f63cc5598afe3
DrDerete <122503880+DrDerete@users.noreply.github.com>
2024-12-08 16:31:04 +0300

    Add files via upload

---
+++ SomeLabs/ThreadLab/Rectangle.java
@@ -0,0 +1,58 @@
+package SomeLabs.ThreadLab;
+
+import java.awt.*;
+
+public class Rectangle extends Canvas implements Runnable {
+
+    private  static final int
+            SLEEP_TIME = 10,
+            WIDTH = 30,
+            HEIGHT = 30;
+
+    private final Color color;
+    private int x, y;
+    private int dx, dy;
+
+    public Rectangle(int velocity, Color color, int x, int y) {
+
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.dx = velocity;
+        this.dy = velocity;
+
+    }
+
+    private void move() {
+
+        if (x > this.getWidth() - WIDTH || x < 0) {
+            dx = -dx;
+        }
+        x += dx;
+
+        if (y > this.getHeight() - HEIGHT || y < 0) {
+            dy = -dy;
+        }
+        y += dy;
+
+    }
+
+    public void run() {
+
+        while (true) {
+            move();
+            repaint();
+            try {
+                Thread.sleep(SLEEP_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(color);
+        g.fillRect(x, y, WIDTH, HEIGHT);
+    }
+}
