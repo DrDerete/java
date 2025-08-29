f51f59e9307a23847e0bacf55de7da161461779e
DeTeTe <Shampunnnnnnnnn@mail.ru>
2025-08-29 15:05:27 +0300

    Java

--- StochasticWay/Main.java
+++
@@ -1,9 +0,0 @@
-package StochasticWay;
-
-import StochasticWay.Entity.Simulation;
-
-public class Main {
-    public static void main(String[] args) {
-        new Simulation().setVisible(true);
-    }
-}
